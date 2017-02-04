2bfcb79e22e97c95989d73b8e3df076acf12bf79
Greg Eng <greg.eng@gmail.com>
2017-02-03 21:26:36 -0700

    property binding and string interpolation of task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './model/task ';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  task1: Task = {
+    content: "Buy some milk",
+    completed: false
+  }
 }
